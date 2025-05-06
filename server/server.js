248bff83bc8e86344ec7f1f245be79f4cb71c8ee
Chriskunda <christianira2003@gmail.com>
2025-05-06 13:22:51 +0200

    Intango Backend Basis

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
+require('dotenv').config()
 const express = require('express');
+const sgMail = require('@sendgrid/mail');
 const cors = require('cors');
 
 const app = express()
 
+const SGAPI = process.env.SENDGRID_API;
+sgMail.setApiKey(SGAPI);
 
 app.use(cors());
 app.use(express.json());
@@ -10,12 +14,29 @@ app.use(express.json());
 
 const PORT = 5000;
 
-app.post('/submit', (req, res) => {
+app.post('/submit', async (req, res) => {
     const name = req.body.name;
     const email = req.body.email;
     const message = req.body.message;
 
-    console.log(req.body);
+    const msg = {
+        to: 'hilldimension.lang@gmail.com',
+        from: 'remyshema20@gmail.com',
+        subject: 'Portfolio Form Submission',
+        html: `    
+            <p>Name: ${name}</p>
+            <p>Email: ${email}</p>
+            <p>Message: ${message}</p>
+        `
+    }
+
+    try {
+        await sgMail.send(msg)
+        res.status(200).send({message: 'Email sent successfully'});
+    } catch(error) {
+        res.status(500).send({error})
+    }
+
 })
 
 app.listen(PORT, () => {
